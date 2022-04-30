2c10cb94439af4104e705dc42b30aaddb9bb86a7
Gaston Escalante <gastonescalante411@gmail.com>
2022-04-30 17:54:49 -0300

    modifique sin nombre para branch 2

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,4 +1,5 @@
-def algunaCosa():
-    print('probamos otra cosa para pushear')
+def algunaCosaDeBranch2():
+    #proebo el branch 2 que cree
+    print('probamos otra cosa para pushear al branch 2')
     #agrego un comentario para pullear
-algunaCosa()
+algunaCosaDeBranch2()

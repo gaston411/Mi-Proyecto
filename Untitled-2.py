33d9922e6799ea4146ac7b90d7632c69f50bff3d
Gaston Escalante <gastonescalante411@gmail.com>
2022-05-12 02:34:28 -0300

    modificacion de busqueda de titulos

--- Untitled-2.py
+++
@@ -1,6 +0,0 @@
-def algunaCosaParaDev():
-    #esta es la rama dev
-    print('probamos otra cosa para pushear')
-    #agrego un comentario para pullear
-
-algunaCosaParaDev()
--- pruebaHola.py
+++
@@ -1,8 +0,0 @@
-# funcion de prueba
-
-def prueba():
-    print("hola")
-    print("de nuevo hola")
-    # comento para ver los cambios
-
-prueba()

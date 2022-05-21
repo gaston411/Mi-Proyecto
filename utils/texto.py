ec03080dd9906d9887e5412a532c175c056e8c9d
Gaston Escalante <gastonescalante411@gmail.com>
2022-05-20 23:32:22 -0300

    cambios generales

---
+++ utils/texto.py
@@ -0,0 +1,13 @@
+# escribe-html-2-windows.py
+
+import webbrowser
+
+f = open('holamundo.html','w')
+
+mensaje = """<html>
+<head></head>
+<body><p>Hola Mundo!</p></body>
+</html>"""
+
+f.write(mensaje)
+f.close()
